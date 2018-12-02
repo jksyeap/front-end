0b4b18f9eda8271ae8af86de2cb5ad7da21adcf9
jksyeap <j.ks.yeap@gmail.com>
2018-12-01 20:57:55 -0800

    instructions in raw markdown now rendered by commonmark

--- src/QuestionPane.js
+++ src/QuestionPane.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import Tab from 'react-bootstrap/lib/Tab';
+var commonmark = require('commonmark');
+var reader = new commonmark.Parser();
+var writer = new commonmark.HtmlRenderer();
 
 class QuestionPane extends React.Component {
   render() {
     let instructions = this.props.tasklist;
     let items = instructions.map(function(element) {
+      let parsed = reader.parse(element["instructions"]);
+      let result = writer.render(parsed);
       return <Tab.Pane eventKey={"#"+element["task-name"]}>
-               {element["instructions"]};
-             </Tab.Pane>
+               <div dangerouslySetInnerHTML={{__html:result}}></div>
+             </Tab.Pane>;
     });
     return <Tab.Content>{items}</Tab.Content>;
   }
